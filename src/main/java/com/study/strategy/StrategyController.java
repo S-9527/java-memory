a337c3e8407548e59324e93f0937bb9cfe00c0c0
kid <2713786149@qq.com>
2026-01-03 14:19:15 +0800

    feat: 扩展代码极为不便

--- src/main/java/com/study/strategy/StrategyController.java
+++ src/main/java/com/study/strategy/StrategyController.java
@@ -10,18 +10,27 @@ public class StrategyController {
     @GetMapping("/{recharge}")
     public String getCustomer(@PathVariable int recharge) {
         if (recharge > 0 && recharge <= 100) {
+            System.out.println(1);
             return "普通玩家客服";
         }
 
         if (recharge > 100 && recharge <= 10000) {
+            System.out.println(1);
             return "小R 玩家客服";
         }
 
         if (recharge > 10000 && recharge <= 100000) {
+            System.out.println(1);
             return "大R 玩家客服";
         }
 
-        if (recharge > 100000) {
+        if (recharge > 100000 && recharge <= 1000000) {
+            System.out.println(1);
+            return "超R 玩家客服";
+        }
+
+        if (recharge > 1000000) {
+            System.out.println(1);
             return "专属客服";
         }
 
