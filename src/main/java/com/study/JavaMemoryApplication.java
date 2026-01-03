1fa88794e23c35b2ce4c62a2c92a0e69d60bc1ee
kid <2713786149@qq.com>
2026-01-03 13:48:52 +0800

    Initial commit

---
+++ src/main/java/com/study/JavaMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMemoryApplication.class, args);
+	}
+
+}
