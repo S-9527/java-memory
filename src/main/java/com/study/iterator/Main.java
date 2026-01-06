086264fbf8ca6f7ca8528f6ff1e41d84034c36bb
kid <2713786149@qq.com>
2026-01-07 01:07:35 +0800

    feat: 添加异常代码

--- src/main/java/com/study/iterator/Main.java
+++ src/main/java/com/study/iterator/Main.java
@@ -1,10 +1,21 @@
 package com.study.iterator;
 
+import java.util.ArrayList;
+
 public class Main {
     static void main() {
-        User user = new User("zhangsan", 3);
-        for (String s : user) {
-            System.out.println(s);
+        ArrayList<User> list = new ArrayList<>();
+        list.add(new User("tom", 11));
+        list.add(new User("jerry", 22));
+
+        for (User user : list) {
+            if (user.getAge() == 11) {
+                list.add(new User("zhangsan", 3));
+            }
+        }
+
+        for (User user : list) {
+            System.out.println(user);
         }
     }
 }
