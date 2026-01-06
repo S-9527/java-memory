535e09b1ff65dad733085984218bda1b0ef4333c
kid <2713786149@qq.com>
2026-01-07 01:19:19 +0800

    feat: 添加案例

--- src/main/java/com/study/iterator/Main.java
+++ src/main/java/com/study/iterator/Main.java
@@ -1,22 +1,30 @@
 package com.study.iterator;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class Main {
-    static void main() {
-        ArrayList<User> list = new ArrayList<>();
-        list.add(new User("tom", 11));
-        list.add(new User("jerry", 22));
-
-        for (User user : list) {
-            if (user.getAge() == 11) {
-                list.add(new User("zhangsan", 3));
-            }
-            break;
-        }
-
-        for (User user : list) {
+    static void main() throws IOException {
+        ArrayList<User> userList = new ArrayList<>();
+        readUsers(user -> {
             System.out.println(user);
+            userList.add(user);
+        });
+    }
+
+    private static void readUsers(Consumer<User> userConsumer) throws IOException {
+        List<String> lines = Files.readAllLines(new File("demo.user").toPath());
+        for (String line : lines) {
+            String midString = line.substring(1, line.length() - 1);
+            String[] split = midString.split(",");
+            String name = split[0];
+            int age = Integer.parseInt(split[1]);
+            User user = new User(name, age);
+            userConsumer.accept(user);
         }
     }
 }
